bfb2daf468d63acf57bfbeaf2b6d4f2ae8906761
wangyongtian <ten951@qq.com>
2019-11-22 14:19:14 +0800

    https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/submissions/

--- src/main/java/com/ten951/algorithm/arraylinked/removeduplicates/Solution.java
+++ src/main/java/com/ten951/algorithm/arraylinked/removeduplicates/Solution.java
@@ -54,7 +54,9 @@ public class Solution {
         int q = 1;
         while (q < nums.length) {
             if (nums[p] != nums[q]) {
-                nums[p + 1] = nums[q];
+                if (q - p > 1) {
+                    nums[p + 1] = nums[q];
+                }
                 p++;
             }
             q++;
