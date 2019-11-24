0519f315153fc76fd3474958dba08e48a8b7c2ef
wangyongtian <ten951@qq.com>
2019-11-24 14:13:10 +0800

    [141]环形链表

---
+++ src/main/java/com/ten951/algorithm/arraylinked/hascycle/Solution.java
@@ -0,0 +1,69 @@
+package com.ten951.algorithm.arraylinked.hascycle;
+
+
+// 示例 1：
+//
+// 输入：head = [3,2,0,-4], pos = 1
+//输出：true
+//解释：链表中有一个环，其尾部连接到第二个节点。
+//
+//
+//
+//
+// 示例 2：
+//
+// 输入：head = [1,2], pos = 0
+//输出：true
+//解释：链表中有一个环，其尾部连接到第一个节点。
+//
+//
+//
+//
+// 示例 3：
+//
+// 输入：head = [1], pos = -1
+//输出：false
+//解释：链表中没有环。
+//
+//
+//
+//
+//
+//
+// 进阶：
+//
+// 你能用 O(1)（即，常量）内存解决此问题吗？
+// Related Topics 链表 双指针
+
+
+/**
+ * @author Darcy
+ * @date 2019-11-24 14:04
+ */
+public class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
