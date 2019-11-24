5b93913f1b01d9f509b8e724c7b97c41586211a6
wangyongtian <ten951@qq.com>
2019-11-24 17:24:25 +0800

    21. 合并两个有序链表

--- src/main/java/com/ten951/algorithm/arraylinked/mergesortlist/Solution.java
+++ src/main/java/com/ten951/algorithm/arraylinked/mergesortlist/Solution.java
@@ -11,8 +11,9 @@ package com.ten951.algorithm.arraylinked.mergesortlist;
 // Related Topics 链表
 
 
-
 //leetcode submit region begin(Prohibit modification and deletion)
+
+import com.ten951.algorithm.arraylinked.hascycle.Solution2;
 
 /**
  * @author Darcy
@@ -30,8 +31,44 @@ public class Solution {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return  null;
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = l1 == null ? l2 : l1;
+        return dummy.next;
     }
 
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(7);
+        node1.next = node2;
+        node2.next = node3;
+
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        ListNode node = new Solution().mergeTwoLists(node1, node4);
+        System.out.println("node = " + node.val);
+    }
 
 }
